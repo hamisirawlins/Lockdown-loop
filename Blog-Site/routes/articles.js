c80d6d091c3c73ec18a9dd07aa7a69374ce9c838
Hamisi Rawlins <53607315+hamisirawlins@users.noreply.github.com>
2020-09-29 20:01:43 +0300

    Fully functional blog, Node,Express, MongoDB

---
+++ Blog-Site/routes/articles.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const article = require('./../models/article')
+const Article = require('./../models/article')
+const router = express.Router()
+
+
+router.get('/new', (req, res) => {
+    res.render('articles/new', { article: new Article() })
+})
+router.get('/edit/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+})
+router.get('/:slug', async(req, res) => { //slug is for making urls more user friendly instead of using original complex ID's
+    const article = await Article.findOne({ slug: req.params.slug })
+    if (article == null) res.redirect('/')
+    res.render('articles/show', { article: article })
+})
+
+router.post('/', async(req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    })
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+        res.render('articles/new', { article: article }) // prefill article with previous data
+    }
+})
+
+router.post('/', async(req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveAndRedirect('new'))
+
+router.put('/:id', async(req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveAndRedirect('edit'))
+
+function saveAndRedirect(path) {
+    return async(req, res) => {
+        let article = req.article
+
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render('articles/${path}', { article: article }) // prefill article with previous data
+        }
+
+    }
+}
+
+router.delete('/:id', async(req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+module.exports = router
